24170bdf5706a99f15162743e8cdffdb424fda53
Alexander Repeta <alexander.repeta@gmail.com>
2023-02-03 20:58:35 +0200

    add examples

--- src/pages/Other.js
+++ src/pages/Other.js
@@ -3,15 +3,25 @@ import axios from 'axios';
 
 export const Other = () => {
   useEffect(() => {
+    const controller = new AbortController();
+
     async function fetchData() {
       try {
-        const response = axios.get(
-          'https://jsonplaceholder.typicode.com/todos'
+        const response = await axios.get(
+          'https://jsonplaceholder.typicode.com/todos',
+          {
+            signal: controller.signal,
+          }
         );
         console.log(response.data);
       } catch (error) {}
     }
+
     fetchData();
+
+    return () => {
+      controller.abort();
+    };
   }, []);
 
   return <main>Other page</main>;
