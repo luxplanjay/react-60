a112f95c1f354cb9c0d2967ca3e1e92f44435232
Alexander Repeta <alexander.repeta@gmail.com>
2023-02-15 17:47:05 +0200

    add examples

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import { Layout } from './Layout';
 import { Home } from 'pages/Home';
-import { Other } from 'pages/Other';
 
 export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
-        <Route path="other" element={<Other />} />
       </Route>
     </Routes>
   );
