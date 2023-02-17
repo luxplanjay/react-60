fb201b932dec17259f6b9b20c3c2cf903f96b2dd
Alexander Repeta <alexander.repeta@gmail.com>
2023-02-17 19:30:38 +0200

    add examples

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,6 @@
-import { Route, Routes } from 'react-router-dom';
 import { Layout } from './Layout';
-import { Home } from 'pages/Home';
 
 export const App = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-      </Route>
-    </Routes>
-  );
+  const [value, setValue] = useState(0);
+  return <Layout>{value}</Layout>;
 };
--- src/pages/Home.js
+++
@@ -1,3 +0,0 @@
-export const Home = () => {
-  return <main>Home page</main>;
-};
