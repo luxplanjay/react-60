6afdee924d1fbdc7db16ef6fbe19d3e4e2860440
Alexander Repeta <alexander.repeta@gmail.com>
2023-03-10 18:01:17 +0200

    add examples

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,21 @@
 import { Layout } from './Layout';
 
 export const App = () => {
-  const [value, setValue] = useState(0);
+  const [contacts, setContacts] = useState([]);
+
+  changeOnlineStatus = contactId => {
+    setContacts(prevContacts =>
+      prevContacts.map(contact => {
+        if (contact.id === contactId) {
+          return {
+            ...contact,
+            isOnline: !contact.isOnline,
+          };
+        }
+        return contact;
+      })
+    );
+  };
+
   return <Layout>{value}</Layout>;
 };
